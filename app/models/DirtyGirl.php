2d657dc53262fd70f3b65ae9099ab0422c4ce51c
Allan Cutler <musicfan03@gmail.com>
2014-01-17 00:59:59 -0800

    Refactored get dirty girls method to fetch correct data and introduced Get_all_girls scoped query.

--- app/models/DirtyGirl.php
+++ app/models/DirtyGirl.php
@@ -4,4 +4,15 @@ class DirtyGirl extends Eloquent {
    * The database table used by the model.
    */
   protected $table = "dirtygirlpages_";
+
+  /**
+   * Scoped queries
+   */
+
+  public function scopeGet_all_girls($query)
+  {
+    return $query
+      ->orderBy('dirty_girl_name','ASC')
+      ->get();
+  }
 }
