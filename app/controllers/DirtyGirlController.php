19076180d1d7adabf5152f59a40d002d28ce0259
Allan Cutler <musicfan03@gmail.com>
2014-01-17 16:29:36 -0800

    Added current voting method. Still tweaking to format payload correctly.

--- app/controllers/DirtyGirlController.php
+++ app/controllers/DirtyGirlController.php
@@ -25,4 +25,44 @@ class DirtyGirlController extends \BaseController {
 
     return Response::json($girls);
   }
+
+  public function current_voting()
+  {
+    $current_poll = VotingPoll::Get_current();
+
+    $poll = [];
+    if($current_poll->count() == 1)
+    {
+      $p = $current_poll->first();
+      $poll['poll']['id'] = $p->id;
+      $poll['poll']['name'] = $p->name;
+      $poll['poll']['question'] = $p->question;
+      $poll['poll']['date_start'] = $p->date_start;
+      $poll['poll']['date_end'] = $p->date_end;
+      $poll['poll']['number_answers'] = $p->number_answers;
+      $poll['poll']['voting_period'] = $p->voting_period;
+      $poll['poll']['created'] = $p->created;
+
+      // Get poll answers
+      $poll['answers'] = [];
+      $answers = VotingPoll::Get_answers($p->id);
+      if($answers->count() > 0)
+      {
+        foreach($answers->get() as $key => $val)
+        {
+          $poll['answers'][$key]['id'] = $val->id;
+          $poll['answers'][$key]['id_poll'] = $val->id_poll;
+          $poll['answers'][$key]['name'] = $val->name;
+          $poll['answers'][$key]['thumbnail'] = $val->thumbnail;
+          $poll['answers'][$key]['username'] = $val->username;
+          $poll['answers'][$key]['caption'] = $val->caption;
+          $poll['answers'][$key]['created'] = $val->created;
+          //Current vote number for answers
+          $poll['answers'][$key]['votes'] = VotingVote::Answer_vote_count($val->id);
+        }
+      }
+    }
+
+    return Response::json($poll);
+  }
 }
