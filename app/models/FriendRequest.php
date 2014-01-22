a20956ad6afa8e601f157355287f1fc22bd4a93c
Allan Cutler <musicfan03@gmail.com>
2014-01-22 08:45:38 -0800

    New friend request scoped query to fetch requests by user id.

--- app/models/FriendRequest.php
+++ app/models/FriendRequest.php
@@ -5,4 +5,26 @@ class FriendRequest extends Eloquent {
    * Table used by model
    */
   protected $table = "community_connection";
+
+  /**
+   * Scoped queries
+   */
+  public function scopeFind_by_id($query, $id)
+  {
+    return $query
+      ->join('users', 'users.id', '=', 'community_connection.connect_from')
+      ->join('community_users', 'users.id', '=', 'community_users.userid')
+      ->where('community_connection.connect_to', '=', $id)
+      ->where('community_connection.status', '=', 0)
+      ->where('users.block', '=', 0)
+      ->orderBy('community_connection.created', 'DESC')
+      ->get([
+        'community_connection.connection_id as id',
+        'users.name',
+        'community_users.alias',
+        'community_users.avatar',
+        'community_users.thumb as thumbnail',
+        'community_connection.msg'
+      ]);
+  }
 }
