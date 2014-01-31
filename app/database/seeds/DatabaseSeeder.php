560ced10555776098eaecbb17bc51f3ca481ebc9
Allan Cutler <musicfan03@gmail.com>
2014-01-30 18:49:29 -0800

    Added user table seeder for seed data. Not using due to repopulated data.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,16 +2,36 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+    $this->call('UserTableSeeder');
+    $this->command->info('User Table Seeded!');
+  }
+}
 
+class UserTableSeeder extends Seeder {
+  public function run()
+  {
+    DB::table('users')->delete();
+
+    User::create([
+      'name' => 'John Doe',
+      'username' => 'john-doe',
+      'email' => 'john@doe.com',
+      'password' => 'TEST',
+      'usertype' => 2,
+      'block' => 0,
+      'registerDate' => date("Y-m-d H:i:s"),
+      'lastvisitDate' => date("Y-m-d H:i:s"),
+      'activation' => 0,
+      'params' => ''
+    ]);
+  }
 }
