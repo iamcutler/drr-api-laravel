961642138f4412f6ab16bcc1b231f766f7bc5eac
Allan Cutler <musicfan03@gmail.com>
2014-02-16 16:02:05 -0800

    Added destroy functionality and a protected function to check user permissions for controller validation.

--- app/controllers/ActivityController.php
+++ app/controllers/ActivityController.php
@@ -46,7 +46,7 @@ class ActivityController extends \BaseController {
 	 */
 	public function show($id)
 	{
-		//
+
 	}
 
 	/**
@@ -79,7 +79,63 @@ class ActivityController extends \BaseController {
 	 */
 	public function destroy($id)
 	{
-		//
+    $input = Input::all();
+    $rules = ['app' => 'required'];
+    $validator = Validator::make($input, $rules);
+    $result = ['result' => false];
+
+    if(!$validator->fails())
+    {
+      $user = $this->user->Find_id_by_hash($input['user_hash']);
+      $activity = $this->activity->find($id);
+
+      if(!is_null($activity))
+      {
+        if($activity->app == $input['app'])
+        {
+          if($this->check_user_permissions($user, $activity))
+          {
+            $activity->delete();
+            $result = ['result' => true];
+          }
+        }
+      }
+    }
+
+    return Response::json($result);
 	}
 
+  protected function check_user_permissions(User $user, Activity $activity)
+  {
+    // Do a switch of the app type to find the right user permissions
+    switch($activity->app)
+    {
+      // Events
+      case 'events.wall':
+        $event = $activity->event();
+        $members = $event->member();
+        foreach($members as $key => $member)
+        {
+          if($member->memberid == $user->id || $member->permission == 1)
+          {
+            return true;
+          }
+        }
+        break;
+      // Groups
+      case 'groups.wall':
+        $group = $activity->group();
+        $members = $group->member();
+        foreach($members as $key => $member)
+        {
+          if($member->memberid == $user->id || $member->permissions == 1)
+          {
+            return true;
+          }
+        }
+        break;
+    }
+
+    return false;
+  }
 }
