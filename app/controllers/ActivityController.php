9c1a855de2f7c79f6b527b3b0a5b023b9db8df75
Allan Cutler <musicfan03@gmail.com>
2014-02-16 21:31:32 -0800

    New function to get an activity instance. Calling that function after an activity save to display in output.

--- app/controllers/ActivityController.php
+++ app/controllers/ActivityController.php
@@ -8,77 +8,129 @@ class ActivityController extends \BaseController {
     $this->user = $user;
   }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Response
+   */
+  public function index()
+  {
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return Response
+   */
+  public function create()
+  {
+    //
+  }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @return Response
+   */
+  public function store()
+  {
+    $input = Input::all();
+    $user = $this->user->Find_id_by_hash($input['user_hash']);
+    $rules = [
+      'event_id' => 'required|integer',
+      'app' => 'required',
+      'actor' => 'required|integer',
+      'message' => 'required'
+    ];
+    $validator = Validator::make($input, $rules);
+    $result = ['result' => false];
 
-	}
+    if(!$validator->fails())
+    {
+      switch($input['app'])
+      {
+        case 'events.wall':
+          $save = $this->activity->create([
+            'actor' => $input['actor'],
+            'target' => 0,
+            'title' => $input['message'],
+            'content' => '',
+            'app' => $input['app'],
+            'verb' => '',
+            'cid' => $input['event_id'],
+            'groupid' => 0,
+            'eventid' => $input['event_id'],
+            'created' => date('Y-m-d h:i:s'),
+            'access' => 0,
+            'params' => '',
+            'archived' => 0,
+            'location' => '',
+            'comment_id' => 0,
+            'comment_type' => $input['app'],
+            'like_id' => 0,
+            'like_type' => $input['app'],
+            'actors' => ''
+          ]);
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+          if($save)
+          {
+            $save->like_id = $save->id;
+            $save->comment_id = $save->id;
+            $save->save();
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+            // Return true and saved record
+            $result['result'] = true;
+            $result['activity'] = $this->get_activity($save);
+          }
+          break;
+      }
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
+    return Response::json($result);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function show($id)
+  {
+
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    //
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function update($id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
     $input = Input::all();
     $rules = ['app' => 'required'];
     $validator = Validator::make($input, $rules);
@@ -103,7 +155,7 @@ class ActivityController extends \BaseController {
     }
 
     return Response::json($result);
-	}
+  }
 
   protected function check_user_permissions(User $user, Activity $activity)
   {
@@ -137,5 +189,67 @@ class ActivityController extends \BaseController {
     }
 
     return false;
+  }
+
+  // Paginate event activity
+  protected function get_activity(Activity $activity)
+  {
+    // Event activity - Initial call will paginate 10 records.
+    $results = [];
+    $key = 0;
+    $comm_user = $activity->actor_comm();
+    $actor = $activity->actor();
+
+    $results[$key]['id'] = $activity->id;
+    $results[$key]['user']['id'] = $actor->id;
+    $results[$key]['user']['name'] = $actor->name;
+    $results[$key]['user']['avatar'] = $comm_user->avatar;
+    $results[$key]['user']['thumbnail'] = $comm_user->thumb;
+    $results[$key]['user']['slug'] = $comm_user->alias;
+
+    $results[$key]['comments'] = [];
+    foreach($activity->wall() as $k => $value)
+    {
+      $user = $value->user();
+      $comm_user = $user->comm_user()->first();
+
+      $results[$key]['comments'][$k]['user']['name'] = $user->name;
+      $results[$key]['comments'][$k]['user']['avatar'] = $comm_user->avatar;
+      $results[$key]['comments'][$k]['user']['thumbnail'] = $comm_user->thumb;
+      $results[$key]['comments'][$k]['user']['slug'] = $comm_user->alias;
+
+      $results[$key]['comments'][$k]['comment'] = $value['comment'];
+      $results[$key]['comments'][$k]['date'] = $value['date'];
+    }
+
+    $results[$key]['app'] = $activity->app;
+    $results[$key]['title'] = $activity->title;
+    $results[$key]['comment_id'] = $activity->comment_id;
+    $results[$key]['comment_type'] = $activity->comment_type;
+    $results[$key]['like_id'] = $activity->like_id;
+    $results[$key]['like_type'] = $activity->like_type;
+    $results[$key]['created'] = $activity->created;
+
+    // Get comment stats
+    $results[$key]['stats'] = [];
+    $results[$key]['stats']['likes'] = $activity->event_likes()->count();
+
+    foreach($activity->event_likes()->get() as $i => $v)
+    {
+      // Likes
+      $user = $v->user_like();
+
+      if(!is_null($user))
+      {
+        $comm_user = $user->comm_user()->first();
+
+        /*$results[$key]['stats']['likes']['user']['name'] = $user->name;
+        $results[$key]['stats']['likes']['user']['avatar'] = $comm_user->avatar;
+        $results[$key]['stats']['likes']['user']['thumbnail'] = $comm_user->thumb;
+        $results[$key]['stats']['likes']['user']['slug'] = $comm_user->alias;*/
+      }
+    }
+
+    return $results;
   }
 }
