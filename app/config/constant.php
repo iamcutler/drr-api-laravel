debed5ebc56fa14e19a32f8e226f5a9fb8b31504
Allan Cutler <musicfan03@gmail.com>
2014-03-11 17:36:17 -0700

    Added AWS url constants

--- app/config/constant.php
+++ app/config/constant.php
@@ -3,6 +3,7 @@
 return [
   'cdn_domain' => 'http://staging.dirtyrottenrides.netdna-cdn.com',
   'AWS' => [
+    'url' => 'https://s3.amazonaws.com/dirty-rotten-rides-prod',
     'bucket' => 'dirty-rotten-rides-prod',
     'key' => 'AKIAIIHSKHGJU53W6A6Q',
     'secret' => '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',
--- app/config/staging/constant.php
+++ app/config/staging/constant.php
@@ -3,6 +3,7 @@
 return [
   'cdn_domain' => 'http://staging.dirtyrottenrides.netdna-cdn.com',
   'AWS' => [
+    'url' => 'https://s3.amazonaws.com/dirty-rotten-rides-staging',
     'bucket' => 'dirty-rotten-rides-staging',
     'key' => 'AKIAIIHSKHGJU53W6A6Q',
     'secret' => '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',
