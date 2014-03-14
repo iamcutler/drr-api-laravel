0a96d260ed60c6c18f45cf5e1b0259c8fbc2cf39
Allan Cutler <musicfan03@gmail.com>
2014-03-14 13:31:16 -0700

    Local dev constants. Current holds AWS credentials and CDN domain.

---
+++ app/config/local/constant.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'cdn_domain' => 'http://staging.dirtyrottenrides.netdna-cdn.com',
+  'AWS' => [
+    'url' => 'https://s3.amazonaws.com/dirty-rotten-rides-staging',
+    'bucket' => 'dirty-rotten-rides-staging',
+    'key' => 'AKIAIIHSKHGJU53W6A6Q',
+    'secret' => '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',
+    'region' => 'us-east-1'
+  ]
+];
