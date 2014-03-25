e9ac0079b208ae95470e62944f677fd0d8086b3a
Allan Cutler <musicfan03@gmail.com>
2014-03-24 17:05:27 -0700

    Added scope query and mass assignment variables for user photo albums

--- app/models/UserPhotoAlbum.php
+++ app/models/UserPhotoAlbum.php
@@ -8,6 +8,12 @@ class UserPhotoAlbum extends Eloquent {
   public $timestamps = false;
 
   /**
+   * Mass Assignment
+   */
+  protected $fillable = ['photoid', 'creator', 'name', 'description', 'permissions', 'created', 'path', 'type', 'groupid', 'hits', 'location', 'latitude', 'longitude', 'default', 'params'];
+  protected $guarded = ['id'];
+
+  /**
    * ORM relations
    */
   public function user_photo_album()
@@ -42,5 +48,13 @@ class UserPhotoAlbum extends Eloquent {
       ->where('creator', '=', $id)
       ->orderBy('created', 'ASC')
       ->get();
+  }
+
+  public function scopeFindMobileAlbum($query, $id)
+  {
+    return $query
+      ->where('creator', '=', $id)
+      ->where('name', '=', 'Mobile Uploads')
+      ->first();
   }
 }
