fdcc752042b48e9c037220d853a8cae1393bf6a3
Allan Cutler <musicfan03@gmail.com>
2014-03-31 14:59:36 -0700

    Added DI for submissions and user and added store functionality to save resource.

--- app/controllers/DirtyGirlSubmissionController.php
+++ app/controllers/DirtyGirlSubmissionController.php
@@ -2,6 +2,12 @@
 
 class DirtyGirlSubmissionController extends \BaseController {
 
+  public function __construct(Submission $submission, User $user)
+  {
+    $this->submission = $submission;
+    $this->user = $user;
+  }
+
   /**
    * Display a listing of the resource.
    *
@@ -19,9 +25,67 @@ class DirtyGirlSubmissionController extends \BaseController {
    */
   public function store()
   {
-    //
-  }
+    $params = Input::all();
+    $user = $this->user->find_id_by_hash($params['user_hash']);
+    $rules = [
+      'first_name' => 'required',
+      'last_name' => 'required',
+      'email' => 'required|email',
+      'phone' => 'required',
+      'where_from' => 'required',
+      'done_pinup' => 'required'
+    ];
+    $validator = Validator::make($params, $rules);
+    $result = [];
+    // Set default status result
+    $result['status'] = false;
 
+    // Check if user was found and parameter validations passed
+    if($validator->passes() && !is_null($user))
+    {
+      try
+      {
+        $save = $this->submission->create([
+          'first_name' => $params['first_name'],
+          'last_name' => $params['last_name'],
+          'email_address' => $params['email'],
+          'phone' => $params['phone'],
+          'age' => $params['age'],
+          'where_are_you_from' => $params['where_from'],
+          'previous_pinup' => $params['done_pinup'],
+          'favorite_car' => $params['favorite_car'],
+          'favorite_pinup' => $params['favorite_pinup'],
+          'special_talent' => $params['special_talent'],
+          'why_you' => $params['why_you'],
+          'biggest_turn_on' => $params['turn_on'],
+          'biggest_turn_off' => $params['turn_off'],
+          'favorite_quote' => $params['favorite_quote'],
+          'image_1' => '',
+          'image_2' => '',
+          'image_3' => '',
+          'image_4' => '',
+          'image_5' => '',
+          'archive' => 0,
+          'created_by' => $user->id,
+          'created_at' => date('Y-m-d H:i:s')
+        ]);
+
+        // Check for successful save of the resource
+        if($save)
+        {
+          // Set result status to true
+          $result['status'] = true;
+          // Display saved resource
+          $result['submission'] = $this->submission->find($save->id);
+        }
+      }
+      catch(Exception $exception) {
+        Log::error($exception);
+      }
+    }
+
+    return Response::json($result);
+  }
 
   /**
    * Display the specified resource.
