23cdab25dd78b0d8f7e24ddb37972fb2f43d752c
Allan Cutler <musicfan03@gmail.com>
2014-04-04 02:33:16 -0700

    Refactored user store action to use validations and new user interface.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,7 +2,7 @@
 
 class UserController extends \BaseController {
 
-  public function __construct(User $user)
+  public function __construct(UserRepositoryInterface $user)
   {
     $this->user = $user;
   }
@@ -18,75 +18,65 @@ class UserController extends \BaseController {
   }
 
   /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
-
-  }
-
-  /**
    * Store a newly created resource in storage.
    *
+   * @param string $name
+   * @param string $username
+   * @param string $email
+   * @param string $password
    * @return Response
    */
   public function store()
   {
     // Registration of a new user
-    $name = Input::get('name');
-    $username = Input::get('username');
-    $email = Input::get('email');
-    $password = Input::get('password');
-    $dob = Input::get('dob');
+    $params = Input::all();
+    $rules = [
+      'name' => 'required',
+      'username' => 'required|unique:users',
+      'email' => 'required|email|unique:users',
+      'password' => 'required|min:6'
+    ];
+    $validator = Validator::make($params, $rules);
+    // Set default status to false
+    $result['status'] = false;
 
-    // Check if username is taken
-    if(!$this->user->Check_username_uniqueness($username)->count())
+    if($validator->passes())
     {
-      // Check if user email adrress has been used
-      if(!$this->user->Check_email_uniqueness($email)->count())
+      $new_user = [
+        'name' => $params['name'],
+        'username' => $params['username'],
+        'email' => $params['email'],
+        'password' => $params['password']
+      ];
+
+      // Call create on user class and return array instance
+      $save = $this->user->create($new_user);
+
+      if(!empty($save))
       {
-        $new_user = [
-          'name' => $name,
-          'username' => $username,
-          'email' => $email,
-          'password' => $this->user->generate_password($password),
-          'usertype' => 2,
-          'registerDate' => date("Y-m-d H:i:s"),
-          'lastvisitDate' => date("Y-m-d H:i:s"),
-          'params' => '',
-          'user_hash' => $this->user->generate_hash($name, $username)
-        ];
+        $result['status'] = true;
+        $result['name'] = $save['name'];
+        $result['username'] = $save['username'];
+        $result['slug'] = $save['id'] .':'. str_replace(' ', '-', $save['name']);
+        $result['hash'] = $save['user_hash'];
 
-        // Save user data
-        $save = $this->user->create($new_user);
-
-        if($save)
-        {
-          // Send confirm / welcome email
-          Email::send('emails.auth.welcome', $new_user, function($message) {
-            $message->to($email, $name)->subject('Welcome to Dirty Rotten Rides');
-          });
-
-          $results = ['status' => true, 'name' => $name, 'username' => $username, 'slug' => $save->id .':'. str_replace(' ', '-', $name), 'hash' => $this->user->Find_hash_by_id($save->id)];
-        }
-        else
-        {
-          $results = ['status' => false, 'code' => '1001', 'message' => 'something went wrong during registration. Please report this bug. Sorry for the inconvenience'];
-        }
+        // Send confirm / welcome email
+        Mail::send('emails.auth.welcome', $new_user, function($message) use ($save) {
+          $message->to($save['email'], $save['name'])->subject('Welcome to Dirty Rotten Rides');
+        });
       }
       else
       {
-        $results = ['status' => false, 'code' => '101', 'message' => 'email address has already been used'];
+        $result['code'] = '1001';
+        $result['message'] = ['Something went wrong during registration. Please report this bug. Sorry for the inconvenience'];
       }
     }
-    else
-    {
-      $results = ['status' => false, 'code' => '100', 'message' => 'username is already taken'];
+    else {
+      $result['code'] = '1002';
+      $result['message'] = $validator->messages()->toArray();
     }
 
-    return Response::json($results);
+    return Response::json($result);
   }
 
   /**
