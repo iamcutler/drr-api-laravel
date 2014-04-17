836aad4258846477144e2eb2262ff394a1a79864
Allan Cutler <musicfan03@gmail.com>
2014-04-17 03:53:34 -0700

    Moved feed payload structure to a feed transformer.

--- app/classes/Presenter.php
+++ app/classes/Presenter.php
@@ -64,4 +64,92 @@ class Presenter implements PresenterRepositoryInterface {
 
     return $result;
   }
+
+  public function profileFeed(Activity $value)
+  {
+    $result = [];
+    // Resource
+    $result['id'] = (int) $value->id;
+    $result['title'] = $value->title;
+    $result['type'] = $value->app;
+    $result['comment_id'] = (int) $value->comment_id;
+    $result['comment_type'] = $value->comment_type;
+    $result['like_id'] = (int) $value->like_id;
+    $result['like_type'] = $value->like_type;
+    $result['created'] = $value->created;
+
+    /// Resource owner
+    $result['user']['id'] = (int) $value->userActor->id;
+    $result['user']['name'] = $value->userActor->name;
+    $result['user']['thumbnail'] = $value->userActor->comm_user->thumb;
+    $result['user']['avatar'] = $value->userActor->comm_user->avatar;
+    $result['user']['slug'] = $value->userActor->comm_user->alias;
+
+    // Resource Target
+    if($value->target == $value->actor || $value->target == 0)
+    {
+      $result['target']['id'] = (int) $value->userActor->id;
+      $result['target']['name'] = $value->userActor->name;
+      $result['target']['thumbnail'] = $value->userActor->comm_user->thumb;
+      $result['target']['avatar'] = $value->userActor->comm_user->avatar;
+      $result['target']['slug'] = $value->userActor->comm_user->alias;
+    }
+    else {
+      $result['target']['id'] = (int) $value->userTarget->id;
+      $result['target']['name'] = $value->userTarget->name;
+      $result['target']['thumbnail'] = $value->userTarget->comm_user->thumb;
+      $result['target']['avatar'] = $value->userTarget->comm_user->avatar;
+      $result['target']['slug'] = $value->userTarget->comm_user->alias;
+    }
+
+    //Resource stats
+    $result['stats']['likes'] = (int) $value->likes()->where('element', '=', $value->like_type)->where('like', '!=', '')->count();
+
+    // Resource comments
+    $result['comments'] = [];
+    foreach($value->wall as $k => $val)
+    {
+      $result['comments'][$k]['user']['id'] = (int) $val->user->id;
+      $result['comments'][$k]['user']['name'] = $val->user->name;
+      $result['comments'][$k]['user']['avatar'] = $val->user->comm_user->avatar;
+      $result['comments'][$k]['user']['thumbnail'] = $val->user->comm_user->thumb;
+      $result['comments'][$k]['user']['slug'] = $val->user->comm_user->alias;
+
+      $result['comments'][$k]['comment'] = $val->comment;
+      $result['comments'][$k]['date'] = $val->date;
+    }
+
+    // Resource media
+    $result['media'] = [];
+    // Output media array based on activity type
+    if($value->app == 'videos')
+    {
+      $media = $value->video;
+
+      if(!is_null($media))
+      {
+        $result['media']['title'] = $media->title;
+        $result['media']['type'] = $media->type;
+        $result['media']['video_id'] = $media->video_id;
+        $result['media']['description'] = $media->description;
+        $result['media']['thumb'] = $media->thumb;
+        $result['media']['path'] = $media->path;
+        $result['media']['created'] = $media->created;
+      }
+    }
+    elseif($value->app == 'photos') {
+      $media = $value->photo;
+
+      if(!is_null($media))
+      {
+        $result['media']['caption'] = $media->caption;
+        $result['media']['image'] = $media->image;
+        $result['media']['thumbnail'] = $media->thumbnail;
+        $result['media']['original'] = $media->original;
+        $result['media']['created'] = $media->created;
+      }
+    }
+
+    return $result;
+  }
 }
--- app/repositories/PresenterRepositoryInterface.php
+++ app/repositories/PresenterRepositoryInterface.php
@@ -5,4 +5,5 @@ interface PresenterRepositoryInterface {
   static function UserImage(UserPhoto $image, Array $options = []);
   static function likeStats($likes);
   static function Wall($wall);
+  public function profileFeed(Activity $value);
 }
