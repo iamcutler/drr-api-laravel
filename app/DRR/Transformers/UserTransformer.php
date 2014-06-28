1c0a3ac7eda874a6bcd312774ebfe91da2f8c7de
Allan Cutler <musicfan03@gmail.com>
2014-06-28 08:39:02 -0700

    Introduced user transformer

---
+++ app/DRR/Transformers/UserTransformer.php
@@ -0,0 +1,16 @@
+<?php namespace DRR\Transformers;
+
+class UserTransformer extends Transformer {
+  public function transform($user)
+  {
+    return [
+      'id' => (int) $user['id'],
+      'name' => $user['name'],
+      'username' => $user['username'],
+      'email' => $user['email'],
+      'thumbnail' => "/{$user['comm_user']['thumb']}",
+      'avatar' => "/{$user['comm_user']['avatar']}",
+      'slug' => $user['comm_user']['alias']
+    ];
+  }
+} 
