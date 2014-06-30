85f6b914818c9af036a77f895fe056ec869f0aa2
Allan Cutler <musicfan03@gmail.com>
2014-06-30 14:06:06 -0700

    Introduced new transformer for user likes to include current requester like status

---
+++ app/DRR/Transformers/UserLikesTransformer.php
@@ -0,0 +1,57 @@
+<?php namespace DRR\Transformers;
+
+class UserLikesTransformer {
+
+  public function transform(array $like, array $user)
+  {
+    return [
+      'likes' => (int) (array_key_exists('like', $like)) ? $this->getCounts($like['like']) : 0,
+      'dislikes' => (int) (array_key_exists('dislike', $like)) ? $this->getCounts($like['dislike']) : 0,
+      'user' => [
+        'like' => (bool) (array_key_exists('like', $like)) ? $this->userStatus($like['like'], $user) : false,
+        'dislike' => (bool) (array_key_exists('dislike', $like)) ? $this->userStatus($like['dislike'], $user ) : false
+      ]
+    ];
+  }
+
+  /**
+   * @param $like
+   * @desc Loop though likes to get current like count
+   * @return int
+   */
+  private function getCounts($like)
+  {
+    // Set initial counter
+    $count = 0;
+
+    if($like != '') {
+      foreach(explode(',', $like) as $val)
+      {
+        $count++;
+      }
+    }
+
+    return $count;
+  }
+
+  /**
+   * @param $like
+   * @param array $user
+   * @desc Find if user has liked/disliked content
+   * @return bool
+   */
+  private function userStatus($like, array $user)
+  {
+    $status = false;
+
+    foreach(explode(',', $like) as $val)
+    {
+      if($val == $user['id'])
+      {
+        $status = true;
+      }
+    }
+
+    return $status;
+  }
+} 
