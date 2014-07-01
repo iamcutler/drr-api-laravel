80c7325f00a0c860ffccd90bef52d03b9a495b2b
Allan Cutler <musicfan03@gmail.com>
2014-07-01 15:23:09 -0700

    Changed likes transformer output array

--- app/DRR/Transformers/LikesTransformer.php
+++ app/DRR/Transformers/LikesTransformer.php
@@ -4,8 +4,9 @@ class LikesTransformer extends Transformer {
   public function transform($like)
   {
     return [
-      'likes' => (int) count(explode(',', $like['like'])),
-      'dislikes' => (int) ($like['dislike'] == '') ? 0 : count(explode(',', $like['dislike']))
+      'uid' => (int) $like['uid'],
+      'likes' => $like['like'],
+      'dislikes' => $like['dislike']
     ];
   }
 } 
