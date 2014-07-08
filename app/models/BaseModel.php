77a3012549e9b5754dfd5d3b518ee9a19aab07ab
Allan Cutler <musicfan03@gmail.com>
2014-07-08 15:31:38 -0700

    Refactored validate method in base model to override attributes if params are passed in to validate for controllers and tests

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -5,9 +5,9 @@ use Illuminate\Database\Eloquent\Model as Eloquent;
 class BaseModel extends Eloquent {
   public $errors;
 
-  public function validate()
+  public function validate($params = [])
   {
-    $v = Validator::make($this->attributes, static::$rules);
+    $v = Validator::make((empty($params)) ? $this->attributes : $params, static::$rules);
     if($v->passes()) return true;
 
     $this->errors = $v->messages();
